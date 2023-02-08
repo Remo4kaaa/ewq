8041e13897589d0fe94ff40508abd81935930cb0
Remo4kaaa <121783354+Remo4kaaa@users.noreply.github.com>
2023-02-08 18:03:29 +0600

    Update tgs3.py

--- tgs3.py
+++ tgs3.py
@@ -12,10 +12,10 @@ chat_id = "@datinganon_bot"
 @app.on_message(chat_id)
 async def spam(client: Client, message: types.Message):
     if "Вам найден собеседник, начинайте диалог." in message.text:  # Меняем это значение если хотим спамить в другом чате
+        sleep(1.3)
+        await app.send_message(chat_id, "Приветик, хочешь трахаться? я очень)")
         sleep(1.5)
-        await app.send_message(chat_id, "Приветик, я девушка, а ты?")
-        sleep(1.5)
-        await app.send_message(chat_id, "Мой парень залил мне полный рот спермы и потом засунул свой.... Хочешь посмотреть?) https://t.me/shoriof  ")
+        await app.send_message(chat_id, "тут я кончила 18 раз от руки хочешь посмотреть?) https://t.me/shoriof  ")
 
         if 'Вам найден собеседник, начинайте диалог.' in message.text:
              sleep(1.5)
